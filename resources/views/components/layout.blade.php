64b0a191f3db202196659efaf3fbe7ada8eeee7a
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-01-24 21:08:07 -0300

    top

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Igreja Prebiteriana da Renovação</title>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('\icone.ico') }}" type="image/x-icon">
 
 </head>
 
