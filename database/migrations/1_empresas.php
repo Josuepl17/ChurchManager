a440f9268b25f0baad6a0ba89d26cf2af57709bc
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-05-02 20:12:38 -0300

    bom

--- database/migrations/1_empresas.php
+++ database/migrations/1_empresas.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
             $table->id();
-            $table->integer('cnpj');
+            $table->string('cnpj', 18);
             $table->text('razao');
             $table->timestamps();
 
