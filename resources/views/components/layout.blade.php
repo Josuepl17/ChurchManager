52ccddfdc36fb63abe7e0b3e35c0d4ca3a1ecc8e
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-06-24 19:12:49 -0300

    Revert "Update layout.blade.php"
    This reverts commit 8c2fb62a8ff16b1208904f241a4d5af156ea291c.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,109 +1,338 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('titulo')</title>
+    <title>@yield('titulo-nav')</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('\icone.ico') }}" type="image/x-icon">
     <style>
-        /* Estilos gerais */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-        .conteiner-geral {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-        .conteiner-titulo {
-            background-color: #f0f0f0;
-            padding: 20px;
-            text-align: center;
-        }
-        .conteiner-menu-pesquisa-conteudo {
-            flex-grow: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        #menutabela-barra {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background-color: #f9f9f9;
-            padding: 20px;
-        }
-        #menu-tabela {
-            display: flex;
-            justify-content: space-between;
-            max-width: 800px;
-            width: 100%;
-        }
-        #menu-esquerdo {
-            flex-basis: 30%;
-            background-color: #fff;
-            padding: 10px;
-            border-right: 1px solid #ddd;
-        }
-        #menu-esquerdo h1 {
-            font-size: 18px;
-            margin-bottom: 10px;
-        }
-        #menu-esquerdo a {
-            display: block;
-            margin-bottom: 5px;
-            text-decoration: none;
-            color: #333;
-        }
-        #menu-esquerdo a:hover {
-            color: #007bff;
-        }
-        .conteiner-tabela {
-            flex-basis: 70%;
-            background-color: #fff;
-            padding: 10px;
-        }
-        .barrainferior {
-            margin-top: 20px;
-            text-align: center;
-            font-size: 14px;
-        }
-        .barrainferior span {
-            font-weight: bold;
-        }
-        #logoff img {
-            vertical-align: middle;
-            margin-right: 5px;
-        }
+        :root {
+
+--titulos: #0a1727;
+--subtitulos: #012841;
+--fundos: #0d8aa6;
+--cor-secundaria: #5353533d;
+}
+
+
+*{
+    box-sizing: border-box;
+}
+
+
+
+.troywell-avia {
+border: none;
+}
+
+.troywell-caa {
+border: none;
+}
+
+h1 {
+font-family: 'Times New Roman', Times, serif;
+color: white;
+}
+
+body {
+background-color: rgba(255, 255, 255, 0.473);
+}
+
+.conteiner-geral {
+display: flex;
+flex-direction: column;
+
+height: 97vh;
+
+
+}
+
+/* Conteiner-Titulo */
+
+
+.conteiner-titulo {
+display: flex;
+width: 100%;
+height: 08%;
+
+justify-content: center;
+align-items: center;
+background-color: var(--titulos);
+border-radius: 10px 10px 0px 0px;
+box-shadow: black 1px 1px 1px;
+border-bottom: solid 1px white;
+
+}
+
+
+#novo_usuario{
+    position: absolute;
+    right: 85px;
+    top: 25px;
+}
+#novo_usuario a{
+
+    background-color: var(--subtitulos);
+    padding-left: 10px;
+    padding-right: 10px;
+    }
+    
+    #novo_usuario a:hover{
+    background-color: var(--fundos);
+    transition: 0.3s;
+    }
+
+
+
+#logout{
+position: absolute;
+right: 29px;
+top: 25px;
+
+
+}
+
+
+
+.conteiner-titulo h1 {
+font-size: 16px;
+font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+}
+
+
+
+/* Conteiner Menu-Pesquisa-Conteudo */
+.conteiner-menu-pesquisa-conteudo {
+display: flex;
+flex-direction: row; /* um do lado do outro */
+justify-content: center;
+height: 92%;
+width: 100%;
+
+
+
+}
+
+#menu-tabela{
+display: flex;
+width: 100%;
+height: 90%;
+flex-direction: row;
+}
+
+
+
+
+/* Menu Lateral Esquerdo*/
+#menu-esquerdo {
+display: flex;
+flex-direction: column;
+width: 18%;
+height: 100%;
+text-align: center;
+border: 1px solid black;
+border-top: none;
+background-color: var(--subtitulos);
+
+
+
+}
+
+
+
+.h1-menu {
+margin-top: 0px;
+width: 100%;
+font-size: 20px;
+background-color: var(--titulos);
+text-align: center;
+
+}
+
+a {
+display: flex;
+justify-content: center;
+align-items: center;
+text-decoration: none;
+width: 100%;
+padding-bottom: 3px;
+padding-top: 3px;
+font-size: 16px;
+color: white;
+font-family: Arial, Helvetica, sans-serif;
+border-bottom: solid 1px rgba(0, 0, 0, 0.529);
+
+}
+
+#menu-esquerdo a:hover {
+color: black;
+background-color: white;
+transition: 0.3s;
+}
+
+
+
+
+
+/* Conteiner-Pesquisa-Conteudo */
+
+
+
+
+
+
+input {
+font-size: 18px;
+width: 20%;
+height: 75%;
+margin-right: 10px;
+border-radius: 5px;
+
+}
+
+/* Tabela*/
+.conteiner-tabela {
+display: flex;
+flex-direction: column;
+border: 1px solid black;
+width: 100%;
+height: 98%;
+background-color: white;
+border: 1px solid black;
+overflow: auto;
+margin-left: 5px;
+
+}
+
+#menutabela-barra{
+display: flex;
+width: 100%;
+height: 100%;
+flex-direction: column;
+
+}
+
+
+
+/* FIM TABELA */
+
+.barrainferior {
+display: flex;
+width: 100%;
+height: 10%;
+border: 1px solid black;
+background-color: var(--titulos);
+align-items: center;
+justify-content: center;
+font-size: 15px;
+color: white;
+border-radius: 0px 0px 10px  10px;
+
+
+}
+
+
+@media(max-width: 640px){
+    .conteiner-geral{
+        height: 90vh;
+    }
+
+
+#menu-esquerdo{
+    display: none;
+    flex-direction: row;
+    width: 100%;
+    height: 10%;
+    align-items: center;
+
+   
+}
+
+.sumir{
+    display: none;
+}
+
+
+
+
+#menu-tabela{
+    flex-direction: column;
+    
+    
+}
+
+.conteiner-tabela{
+    width: 100%;
+    height: 100%;
+    margin-left: 0px;
+}
+
+.h1-menu{
+    display: none;
+}
+
+a{
+    border-bottom: none;
+    
+    border-right: 1px solid black;
+    
+    
+    
+}
+
+
+}
     </style>
 </head>
+
 <body>
+
     <div class="conteiner-geral">
+
         <div class="conteiner-titulo">
             <h1>@yield('titulo')</h1>
         </div>
+
+
         <main class="conteiner-menu-pesquisa-conteudo">
+
             <div id="menutabela-barra">
                 <div id="menu-tabela">
                     <div id="menu-esquerdo">
                         <h1 class="h1-menu">Menu</h1>
                         <a href="/">Home</a>
+                        
                         <a href="/oferta">Cadastro Oferta</a>
                         <a href="/despesas">Cadastro Despesas</a>
-                        <a href="/relatorio">Relatórios</a>
+                        <a href="/relatorio">Relatorios</a>
                         <a href="/indexcaixa">Caixa</a>
-                        <a href="/user/profile">Usuários</a>
-                        <a href="/cadastro/login/novo">Novo Usuário</a>
-                        <a id="logoff" href="/logout"><img src="{{ asset('\sair.png') }}" alt="">&nbsp;Logout</a>
+                        <a href="/user/profile">Usuarios</a>
+                        <a href="/cadastro/login/novo">Novo Usuario</a>
+                        <a id="logoff" href="/logout"><img  src="{{ asset('\sair.png') }}" alt="">&nbspLogout</a>
                     </div>
                     <div class="conteiner-tabela">
                         @yield('conteudo')
                     </div>
                 </div>
-                <div class="barrainferior">Usuário:&nbsp;<span>{{ Auth::User()->user }}</span></div>
+
+
+                <div class="barrainferior">Usuário:&nbsp<span>{{ Auth::User()->user }}</span></div>
             </div>
+
         </main>
+
     </div>
+
+
+
+
+
+
+
+
 </body>
+
 </html>
